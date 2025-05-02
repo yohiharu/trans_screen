8b136fa364f3ccacd8d9369d6a4df195b7ec1b77
yohiharu <manjinnji@gmail.com>
2025-05-02 11:45:11 +0900

    Use gcp

--- main.py
+++ main.py
@@ -4,51 +4,75 @@ import pyocr
 import re
 from dotenv import load_dotenv
 from openai import OpenAI
+import os
+from google.cloud import vision
+
 
 load_dotenv()
-client = OpenAI()
 
 
-file_name = 'my_screenshot.png'
+
+file_name = "my_screenshot.png"
 wh = pyautogui.size()
 
-pyautogui.screenshot(file_name, region=(0, int(wh.height/10), int(wh.width/2), int(wh.height * 8 /10))) 
+pyautogui.screenshot(
+    file_name,
+    region=(0, int(wh.height / 10), int(wh.width / 2), int(wh.height * 8 / 10)),
+)
 img1 = Image.open(file_name)
 
-pyocr.tesseract.TESSERACT_CMD = 'C:/Users/81704/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
 
 
+
+
+username = os.getenv("USERNAME")
+pyocr.tesseract.TESSERACT_CMD = (
+    "C:/Users/{}/AppData/Local/Programs/Tesseract-OCR/tesseract.exe".format(username)
+)
 tools = pyocr.get_available_tools()
 tool = tools[0]
-
 txt1 = tool.image_to_string(
-    img1,
-    lang='jpn+eng',
-    builder=pyocr.builders.TextBuilder(tesseract_layout=6)
+    img1, lang="jpn+eng", builder=pyocr.builders.TextBuilder(tesseract_layout=6)
 )
-pattern = r'[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f\u3000 a-zA-Z0-9。、,\.]'
-
+pattern = r"[\u3040-\u30ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff\uff66-\uff9f\u3000 a-zA-Z0-9。、,\.]"
 result = re.findall(pattern, txt1)
-
 result = "".join(result)
-print(result)
 
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./key.json"
+
+client = vision.ImageAnnotatorClient()
+with open(file_name, "rb") as image_file:
+    content = image_file.read()
+image = vision.Image(content=content)
+response = client.text_detection(image=image)
+
+for page in response.full_text_annotation.pages:
+    for block in page.blocks:
+        block_text = ""
+        for paragraph in block.paragraphs:
+            for word in paragraph.words:
+                word_text = ''.join([symbol.text for symbol in word.symbols])
+                block_text += word_text + ' '
+        print("Block:")
+        print(block_text.strip())
+        vertices = block.bounding_box.vertices
+        print(vertices)
+        print("-" * 20)
+
+"""
+client = OpenAI()
 completion = client.chat.completions.create(
     model="gpt-4.1",
     messages=[
         {
             "role": "user",
-            "content": "あなたは日本語の文章を英語に翻訳するプロの翻訳者です。以下の日本語の文章を英語に翻訳してください。翻訳結果以外は一切必要ありません。\n\n" + result
+            "content": "あなたは日本語の文章を英語に翻訳するプロの翻訳者です。以下の日本語の文章を英語に翻訳してください。翻訳結果以外は一切必要ありません。\n\n"
+            + result,
         }
-    ]
+    ],
 )
-
 print()
 print(completion.choices[0].message.content)
 
-
-tool.image_to_string(
-    img1,
-    lang='jpn+eng',
-    builder=pyocr.builders.TextBuilder(tesseract_layout=6)
-)
+"""
